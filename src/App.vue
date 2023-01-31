500664ac3685dc3ee6c936435c9c85d1fe966e73
GuglielmoBini <guglielmobini@gmail.com>
2023-01-31 13:07:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+    name: 'App',
+    components: { AppHeader, AppMain, AppFooter }
+}
+</script>
+
+<template>
+    <app-header></app-header>
+    <app-main></app-main>
+    <app-footer></app-footer>
+</template>
+
+<style lang="scss">
+// importo style.scss
+@use './assets/scss/style.scss';
+</style>
