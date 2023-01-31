644ef9005157da7c8849e99526e20faa6ff96c99
GuglielmoBini <guglielmobini@gmail.com>
2023-01-31 15:45:03 +0100

    created arrays

---
+++ src/data/index.js
@@ -0,0 +1,62 @@
+// array dati
+navLinks = [
+  "home",
+  "about",
+  "prices",
+  "courses",
+  "locations",
+  "blog",
+  "book now",
+];
+
+instructors = [
+  {
+    name: "Mike Hart",
+    src: "./src/assets/img/instructors/instructor-mikehart-600x381.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "John Smith",
+    src: "./src/assets/img/instructors/instructor-johnsmith-600x381.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    name: "Angela Hart",
+    src: "./src/assets/img/instructors/instructor-angelahart-600x381.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
+testimonials = [
+  {
+    name: "Sophia",
+    src: "./src/assets/img/testimonials/testimonial-sophia.png",
+  },
+  {
+    name: "Kelly",
+    src: "./src/assets/img/testimonials/testimonial-kelly.png",
+  },
+  {
+    name: "Kate",
+    src: "./src/assets/img/testimonials/testimonial-kate.png",
+  },
+  {
+    name: "Harold",
+    src: "./src/assets/img/testimonials/testimonial-harold.png",
+  },
+  {
+    name: "Grant",
+    src: "./src/assets/img/testimonials/testimonial-grant.png",
+  },
+];
+
+contactDetails = [
+  {
+    address: "12345 North Main Street, New York, NY 555555",
+    phone: "1.800.555.6789",
+    mail: "info@your-domain.com",
+    link: "Theme-Fusion.com",
+  },
+];
+
+courses = ["Pass Plus", "Intensive Course", "Automatic", "Instructor Training"];
